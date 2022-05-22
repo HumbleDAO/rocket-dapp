369777e43ea0b942b0cee092317d5c6504aad84f
Hugo Cedano <hugoce17@gmail.com>
2022-05-21 22:36:08 -0500

    fixed websocket issues for contracts while local development

--- src/plugins/web3.js
+++ src/plugins/web3.js
@@ -2,30 +2,23 @@ import Web3 from 'web3'
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 export default function ({ $config: { networkData } }, inject) {
-  try {
-    const web3 = new Web3(networkData[0].rpcUrls[0])
-    console.log('INIT_WEB3: ', web3)
+  const web3 = new Web3(networkData[0].rpcUrls[0])
+  console.log('INIT_WEB3: ', web3)
 
-    const web3Socket = new Web3(
-      new Web3.providers.WebsocketProvider(networkData[0].socketRpcUrls[0]),
-      {
-        // Enable auto reconnection
-        reconnect: {
-          auto: true,
-          delay: 5000, // ms
-          maxAttempts: 20,
-          onTimeout: false,
-        },
-      }
-    )
+  const web3Socket = new Web3(
+    new Web3.providers.WebsocketProvider(networkData[0].socketRpcUrls[0]),
+    {
+      // Enable auto reconnection
+      reconnect: {
+        auto: true,
+        delay: 5000, // ms
+        maxAttempts: 20,
+        onTimeout: false,
+      },
+    }
+  )
 
-    inject('web3', web3)
-    inject('web3Socket', web3Socket)
-    console.log('INIT_WEB3_SOCKET: ', web3Socket)
-  } catch (error) {
-    console.warn(
-      'web3 or web3socket initialization failed: ',
-      new Web3(networkData[0].rpcUrls[0])
-    )
-  }
+  inject('web3', web3)
+  inject('web3Socket', web3Socket)
+  console.log('INIT_WEB3_SOCKET: ', web3Socket)
 }
